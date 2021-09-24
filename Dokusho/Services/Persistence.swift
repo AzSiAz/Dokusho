3ada2b4c16f2d8dfdd45f6c196d21273846c0d04
AzSiAz <stephan@deumier.dev>
2021-09-24 14:42:12 +0200

    Revert to regular core data
    Only for now, until I figure out why sync is still not working

--- Dokusho/Services/Persistence.swift
+++ Dokusho/Services/Persistence.swift
@@ -38,10 +38,10 @@ typealias BackupResult = Result<BackupTask, Error>
 class PersistenceController {
     static let shared = PersistenceController()
 
-    let container: NSPersistentCloudKitContainer
+    let container: NSPersistentContainer
 
     init() {
-        container = NSPersistentCloudKitContainer(name: "Dokusho")
+        container = NSPersistentContainer(name: "Dokusho")
         
         guard let storeDescription = container.persistentStoreDescriptions.first else { fatalError("Failed to open first persistentStoreDescription") }
         storeDescription.setOption(true as NSNumber, forKey: NSPersistentStoreRemoteChangeNotificationPostOptionKey)
