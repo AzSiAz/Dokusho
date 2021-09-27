665afc8f92a7ac6cffaf46f3fa1a974359227134
AzSiAz <stephan@deumier.dev>
2021-09-27 16:23:58 +0200

    library tab can add, delete and change position for CollectionEntity

--- Dokusho/Views/LibraryTabView/LibraryTabView.swift
+++ Dokusho/Views/LibraryTabView/LibraryTabView.swift
@@ -9,10 +9,16 @@ import SwiftUI
 import SwiftUIX
 
 struct LibraryTabView: View {
+    @Environment(\.managedObjectContext) var ctx
+    
     @FetchRequest(sortDescriptors: [CollectionEntity.positionOrder], predicate: nil, animation: .default)
     var collections: FetchedResults<CollectionEntity>
 
     @StateObject var vm: LibraryVM = .init()
+
+    @State var editMode: EditMode = .inactive
+    @State var newCollectionName = ""
+    @FocusState var newCollectionFocus: Bool
     
     var body: some View {
         NavigationView {
@@ -22,40 +28,63 @@ struct LibraryTabView: View {
                         Label("\(collection.getName()) (\(collection.mangas?.count ?? 0))", systemImage: "square.grid.2x2")
                     }
                 }
-            }
-            .toolbar(content: { AddButton(onTapGesture: { print("test") }) })
-            .navigationTitle("Collections")
-        }
-    }
-}
-
-struct CollectionPage: View {
-    @ObservedObject var collection: CollectionEntity
-    @FetchRequest var mangas: FetchedResults<MangaEntity>
-    @State var selectedManga: MangaEntity?
-    @State var showFilter = false
-    
-    var columns: [GridItem] = [GridItem(.adaptive(minimum: 120, maximum: 120))]
-    
-    init(collection: CollectionEntity) {
-        self._collection = .init(wrappedValue: collection)
-        self._mangas = .init(sortDescriptors: [MangaEntity.lastUpdate], predicate: MangaEntity.collectionPredicate(collection: collection), animation: .easeIn)
-    }
-    
-    var body: some View {
-        ScrollView {
-            LazyVGrid(columns: columns) {
-                ForEach(mangas) { manga in
-                    MangaCardView(manga: manga)
-                        .onTapGesture { selectedManga = manga }
+                .onDelete(perform: onDelete)
+                .onMove(perform: onMove)
+                
+                if editMode.isEditing {
+                    TextField(text: $newCollectionName)
+                        .focused($newCollectionFocus)
+                        .submitLabel(.done)
+                        .onSubmit(saveNewCollection)
                 }
             }
+            .id(editMode)
+            .toolbar {
+                ToolbarItemGroup(placement: .navigationBarTrailing) {
+                    EditButton()
+                }
+            }
+            .navigationTitle("Collections")
+            .environment(\.editMode, $editMode)
         }
-        .sheetSizeAware(item: $selectedManga, content: { manga in
-            MangaDetailView(mangaId: manga.mangaId!, src: Int(manga.source!.sourceId))
-        })
-        .toolbar { LibraryToolbarView(collection: collection, showFilter: $showFilter) }
-        .navigationTitle("\(collection.name ?? "No name") (\(mangas.count))")
-        .navigationBarTitleDisplayMode(.inline)
+    }
+    
+    func saveNewCollection() {
+        let lastPosition = (collections.last?.position ?? 0) + 1
+        
+        try? ctx.performAndWait {
+            let _ = CollectionEntity(ctx: ctx, name: newCollectionName, position: Int(lastPosition))
+            try ctx.save()
+        }
+        
+        newCollectionName = ""
+    }
+    
+    func onDelete(_ offsets: IndexSet) {
+        offsets
+            .map { collections[$0] }
+            .forEach { collection in
+                ctx.perform {
+                    ctx.delete(collection)
+                }
+            }
+    }
+    
+    func onMove(_ offsets: IndexSet, _ position: Int) {
+        try? ctx.performAndWait {
+            var revisedItems: [ CollectionEntity ] = collections.map{ $0 }
+
+            // change the order of the items in the array
+            revisedItems.move(fromOffsets: offsets, toOffset: position )
+
+            // update the userOrder attribute in revisedItems to
+            // persist the new order. This is done in reverse order
+            // to minimize changes to the indices.
+            for reverseIndex in stride(from: revisedItems.count - 1, through: 0, by: -1) {
+                revisedItems[reverseIndex].position = Int16(reverseIndex)
+            }
+            
+            try ctx.save()
+        }
     }
 }
---
+++ Dokusho/Views/LibraryTabView/Pages/CollectionPage.swift
@@ -0,0 +1,40 @@
+//
+//  ManageCollectionsModal.swift
+//  Dokusho (iOS)
+//
+//  Created by Stephan Deumier on 26/06/2021.
+//
+
+import SwiftUI
+
+struct CollectionPage: View {
+    @FetchRequest var mangas: FetchedResults<MangaEntity>
+
+    @ObservedObject var collection: CollectionEntity
+    @State var selectedManga: MangaEntity?
+    @State var showFilter = false
+    
+    var columns: [GridItem] = [GridItem(.adaptive(minimum: 120, maximum: 120))]
+    
+    init(collection: CollectionEntity) {
+        self._collection = .init(wrappedValue: collection)
+        self._mangas = .init(sortDescriptors: [MangaEntity.lastUpdate], predicate: MangaEntity.collectionPredicate(collection: collection), animation: .easeIn)
+    }
+    
+    var body: some View {
+        ScrollView {
+            LazyVGrid(columns: columns) {
+                ForEach(mangas) { manga in
+                    MangaCardView(manga: manga)
+                        .onTapGesture { selectedManga = manga }
+                }
+            }
+        }
+        .sheetSizeAware(item: $selectedManga, content: { manga in
+            MangaDetailView(mangaId: manga.mangaId!, src: Int(manga.source!.sourceId))
+        })
+        .toolbar { LibraryToolbarView(collection: collection, showFilter: $showFilter) }
+        .navigationTitle("\(collection.name ?? "No name") (\(mangas.count))")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
