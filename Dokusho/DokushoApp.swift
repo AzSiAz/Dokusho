9aafab9b280b7682c244a5e0606a9f8b148342f9
AzSiAz <stephan@deumier.dev>
2022-04-16 20:38:33 +0200

    Add Hot Reload on dev

--- Dokusho/DokushoApp.swift
+++ Dokusho/DokushoApp.swift
@@ -7,16 +7,22 @@
 
 import SwiftUI
 import TelemetryClient
+import Inject
 
 @main
 struct DokushoApp: App {
+    
+    @ObservedObject private var iO = Inject.observer
+    
     init() {
         TelemetryManager.initialize(with: TelemetryManagerConfiguration.init(appID: "B004B7C1-9A6A-42BF-8234-1B21FC94C6DF"))
     }
     
     var body: some Scene {
         WindowGroup {
-            RootView().environment(\.appDatabase, .shared)
+            RootView()
+                .environment(\.appDatabase, .shared)
+                .enableInjection()
         }
     }
 }
