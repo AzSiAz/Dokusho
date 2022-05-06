e50931df9d5673b700e80d0113be211eca4f9e6a
AzSiAz <stephan@deumier.dev>
2022-05-07 01:18:19 +0200

    .

--- Dokusho/DokushoApp.swift
+++ Dokusho/DokushoApp.swift
@@ -11,7 +11,6 @@ import TelemetryClient
 @main
 struct DokushoApp: App {
     @StateObject var libraryUpdater = LibraryUpdater()
-    @ObservedObject private var iO = Inject.observer
     
     init() {
         TelemetryManager.initialize(with: TelemetryManagerConfiguration.init(appID: "B004B7C1-9A6A-42BF-8234-1B21FC94C6DF"))
